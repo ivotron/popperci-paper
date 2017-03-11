726c54a78df1191b3573d582046a19772214ed26
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-03-11 00:30:45 -0800

    Update experiment to make use of submodules

--- experiments/spark-perf/run.sh
+++ experiments/spark-perf/run.sh
@@ -2,26 +2,21 @@
 set -e -x
 
 # delete previous results
-sudo rm -rf /tmp/results/
-sudo rm -rf results/
+rm -fr results/benchoutput/*
+rm -fr results/facts/*
 
-libltdl=`ldd $(which docker) | grep ltdl | sed 's/.*\( .*libltdl.so.* \).*/\1/' | sed 's/ //g'`
-if [ -n "$libltdl" ] ; then
-  libltdl="-v $libltdl:$libltdl"
-fi
-
+mkdir -p $outdir/
 docker run --rm -ti \
   -v `pwd`/ansible:/experiment \
-  -v `pwd`/../../vendor/baseliner:/experiment/roles/baseliner \
-  -v /tmp:/tmp \
-  -v $HOME/.ssh/issdm_rsa:/root/.ssh/id_rsa \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /usr/bin/docker:/usr/bin/docker \
-  $libltdl \
+  -v `pwd`/vars.yml:/experiment/vars.yml \
+  -v `pwd`/results/benchoutput:/results \
+  -v $SSH_AUTH_SOCK:/ssh-agent \
+  -e SSH_AUTH_SOCK=/ssh-agent \
   --workdir=/experiment \
   --net=host \
   --entrypoint=/bin/bash \
   ivotron/ansible:2.2.0.0 -c \
-    "ansible-playbook -e results_path=/tmp/results -e @vars.yml playbook.yml"
-
-sudo mv /tmp/results results
+    "ansible-playbook \
+      -e @vars.yml \
+      -e local_results_path=/results \
+      playbook.yml"
